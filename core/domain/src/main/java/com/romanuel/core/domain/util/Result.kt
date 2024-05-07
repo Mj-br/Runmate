fb8d14b92679c2e693d4510078b33b6106cfcad8
Manuel Rodríguez <98741174+Mj-br@users.noreply.github.com>
2024-05-07 22:22:45 +0200

    Add Data wrapper

---
+++ core/domain/src/main/java/com/romanuel/core/domain/util/Result.kt
@@ -0,0 +1,16 @@
+package com.romanuel.core.domain.util
+
+sealed interface Result<out D, out E : Error> {
+    data class Success<out D>(val data: D) : Result<D, Nothing>
+    data class Error<out E : com.romanuel.core.domain.util.Error>(val error: E) : Result<Nothing, E>
+}
+
+inline fun <T, E: Error, R> Result<T, E>.map(transform: (T) -> R): Result<R, E> = when (this) {
+    is Result.Success -> Result.Success(transform(data))
+    is Result.Error -> Result.Error(error)
+}
+
+//Transform data into Unit
+fun <T, E: Error> Result<T, E>.asEmptyDataResult(): EmptyDataResult<E> = map{ }
+
+typealias EmptyDataResult<E> = Result<Unit, E>
